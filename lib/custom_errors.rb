7b81ff80d9d5e3d65ae1ea3eb151fdb4977a5313
Huda Yousif <hjdoski@gmail.com>
2019-10-22 20:07:28 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -17,5 +17,11 @@ beyonce.get_married("Jay-Z")
 puts beyonce.name
 
 
-
+  class PartnerError < StandardError
+  end
+end
+ 
+beyonce = Person.new("Beyonce")
+beyonce.get_married("Jay-Z")
+puts beyonce.name
 
