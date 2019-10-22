13f14ad0892bf90f2e3de9e9e1df53dff3c6a1a7
Huda Yousif <hjdoski@gmail.com>
2019-10-22 20:09:14 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,10 +1,10 @@
 class Person
   attr_accessor :partner, :name
-
+ 
   def initialize(name)
     @name = name
   end
-
+ 
   def get_married(person)
     self.partner = person
     if person.class != Person
@@ -13,14 +13,7 @@ class Person
       person.partner = self
     end
   end
-
-end
-
-beyonce = Person.new("Beyonce")
-beyonce.get_married("Jay-Z")
-puts beyonce.name
-
-
+ 
   class PartnerError < StandardError
   end
 end
@@ -28,4 +21,3 @@ end
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
