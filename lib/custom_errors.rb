302d233f5a2f0fc73419449ffd39fbe0b6d47e03
Huda Yousif <hjdoski@gmail.com>
2019-10-22 20:13:14 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -5,14 +5,18 @@ class Person
     @name = name
   end
  
-  def get_married(person)
+def get_married(person)
     self.partner = person
     if person.class != Person
-      raise PartnerError
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+          puts error.message
+      end
     else
       person.partner = self
     end
-  end
+end
  
   class PartnerError < StandardError
   def message
